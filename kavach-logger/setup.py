cb043c7a94247243e081cf923025ab8579b52b2b
Shivam Namdeo <52598179+shivamnamdeo0101@users.noreply.github.com>
2026-06-13 10:31:20 +0530

    Update setup.py

--- kavach-logger/setup.py
+++ kavach-logger/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kavach-logger",
-    version="0.1.0",
+    version="0.2.0",
     description="Centralized logging and masking for Kavach security suite",
     author="Shivam Namdeo",
     author_email="shivamnamdeo0101@gmail.com",
